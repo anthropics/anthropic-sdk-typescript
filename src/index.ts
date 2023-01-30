fe3dee6c92cdddb8f0c17c2e8f50d2feede74555
Justin Spahr-Summers <justin@anthropic.com>
2023-01-30 15:29:54 +0000

    Anthropic SSE client

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import { fetchEventSource } from "@fortaine/fetch-event-source";
+
 export type StreamingMessage = {
   completion: string;
   stop: string | null;
@@ -6,3 +8,85 @@ export type StreamingMessage = {
   exception: string | null;
   log_id: string;
 };
+
+export type SamplingParameters = {
+  prompt: string;
+  model: string;
+  stop_sequences: string[];
+  max_tokens_to_sample: number;
+};
+
+export type OnSampleChange = (sample: string) => void | Promise<void>;
+
+const CLIENT_ID = "anthropic-typescript/0.2.0";
+const DEFAULT_API_URL = "https://api.anthropic.com";
+
+enum Event {
+  Ping = "ping",
+}
+
+export class Client {
+  private apiUrl: string;
+
+  constructor(private apiKey: string, { apiUrl }: { apiUrl?: string }) {
+    this.apiUrl = apiUrl ?? DEFAULT_API_URL;
+  }
+
+  sample(
+    params: SamplingParameters,
+    onSampleChange: OnSampleChange
+  ): Promise<string> {
+    const abortController = new AbortController();
+    return new Promise((resolve, reject) => {
+      fetchEventSource("https://api.anthropic.com/v1/complete", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          Client: CLIENT_ID,
+          "X-API-Key": this.apiKey,
+        },
+        body: JSON.stringify({
+          ...params,
+          stream: true,
+        }),
+        signal: abortController.signal,
+        onopen: async (response) => {
+          if (!response.ok) {
+            abortController.abort();
+            return reject(
+              Error(
+                `Failed to open sampling stream, HTTP status code ${response.status}: ${response.statusText}`
+              )
+            );
+          }
+        },
+        onmessage: (ev) => {
+          if (ev.event === Event.Ping) {
+            return;
+          }
+
+          const message = JSON.parse(ev.data) as StreamingMessage;
+          if (message.stop_reason !== null) {
+            abortController.abort();
+            return resolve(message.completion);
+          }
+
+          if (onSampleChange) {
+            Promise.resolve(onSampleChange(message.completion)).catch(
+              (error) => {
+                abortController.abort();
+                reject(error);
+              }
+            );
+          }
+        },
+        onerror: (error) => {
+          console.error("Sampling error:", error);
+          abortController.abort();
+          return reject(error);
+        },
+      });
+    });
+  }
+}
