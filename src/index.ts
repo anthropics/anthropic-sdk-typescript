aa9fa2e7a78a101d0d4ea29bbce06e4506d73b64
Justin Spahr-Summers <justin@anthropic.com>
2023-02-08 10:00:47 +0000

    Bump package version

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ export type OnUpdate = (completion: CompletionResponse) => void | Promise<void>;
 export const HUMAN_PROMPT = "\n\nHuman:";
 export const AI_PROMPT = "\n\nAssistant:";
 
-const CLIENT_ID = "anthropic-typescript/0.3.1";
+const CLIENT_ID = "anthropic-typescript/0.4.0";
 const DEFAULT_API_URL = "https://api.anthropic.com";
 
 enum Event {
