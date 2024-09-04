c7557f14ece5a693505c27ccf562e06472ab60f1
Robert Craigie <robert@craigie.dev>
2024-09-04 18:52:00 +0100

    chore(api): deprecate claude-1 models

--- src/resources/messages.ts
+++ src/resources/messages.ts
@@ -32,6 +32,13 @@ export class Messages extends APIResource {
     body: MessageCreateParams,
     options?: Core.RequestOptions,
   ): APIPromise<Message> | APIPromise<Stream<RawMessageStreamEvent>> {
+    if (body.model in DEPRECATED_MODELS) {
+      console.warn(
+        `The model '${body.model}' is deprecated and will reach end-of-life on ${
+          DEPRECATED_MODELS[body.model]
+        }\nPlease migrate to a newer model. Visit https://docs.anthropic.com/en/docs/resources/model-deprecations for more information.`,
+      );
+    }
     return this._client.post('/v1/messages', {
       body,
       timeout: (this._client as any)._options.timeout ?? 600000,
@@ -220,6 +227,18 @@ export type Model =
   | 'claude-2.1'
   | 'claude-2.0'
   | 'claude-instant-1.2';
+
+type DeprecatedModelsType = {
+  [K in Model]?: string;
+};
+
+const DEPRECATED_MODELS: DeprecatedModelsType = {
+  'claude-1.3': 'November 6th, 2024',
+  'claude-1.3-100k': 'November 6th, 2024',
+  'claude-instant-1.1': 'November 6th, 2024',
+  'claude-instant-1.1-100k': 'November 6th, 2024',
+  'claude-instant-1.2': 'November 6th, 2024',
+};
 
 export interface RawContentBlockDeltaEvent {
   delta: TextDelta | InputJSONDelta;
