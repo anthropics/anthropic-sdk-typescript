f7ba44fa51cf1fd99e6f677e5c09d026cac1ad69
stainless-em <em@stainless.com>
2025-02-25 15:58:40 -0500

    fix(tests): migrate last ReadableStreamFrom

--- tests/streaming.test.ts
+++ tests/streaming.test.ts
@@ -226,7 +226,10 @@ test('error handling', async () => {
     yield Buffer.from('\n\n');
   }
 
-  const stream = Stream.fromSSEResponse(new Response(await iteratorToStream(body())), new AbortController());
+  const stream = Stream.fromSSEResponse(
+    new Response(await ReadableStreamFrom(body())),
+    new AbortController(),
+  );
 
   const err = expect(
     (async () => {
