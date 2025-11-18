aec4cce1bf7c269e93299af52efdac21f0aa957d
Stephen <stephen@stainless.com>
2025-11-18 15:51:29 -0500

    don't strip path from filename in multipart

--- src/internal/uploads.ts
+++ src/internal/uploads.ts
@@ -51,17 +51,13 @@ export function makeFile(
 
 export function getName(value: any): string | undefined {
   return (
-    (
-      (typeof value === 'object' &&
-        value !== null &&
-        (('name' in value && value.name && String(value.name)) ||
-          ('url' in value && value.url && String(value.url)) ||
-          ('filename' in value && value.filename && String(value.filename)) ||
-          ('path' in value && value.path && String(value.path)))) ||
-      ''
-    )
-      .split(/[\\/]/)
-      .pop() || undefined
+    (typeof value === 'object' &&
+      value !== null &&
+      (('name' in value && value.name && String(value.name)) ||
+        ('url' in value && value.url && String(value.url)) ||
+        ('filename' in value && value.filename && String(value.filename)) ||
+        ('path' in value && value.path && String(value.path)))) ||
+    ''
   );
 }
 
